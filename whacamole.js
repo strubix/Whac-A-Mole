a19f6a5d2e50530399773a9a89434fa6e6ab9f6b
Lucas <lucas.strubi@gmail.com>
2016-07-28 23:38:51 +0200

    game menu concept

--- whacamole.js
+++ whacamole.js
@@ -7,8 +7,49 @@ function Game(selector){
     selector.css({
         width: '600px',
         height: '800px',
-        'background': 'black',
-        margin: '0 auto'
+        background: 'black',
+        margin: '0 auto',
+        position: 'relative'
+    });
+
+    selector.append('<h2 id="game_title">Whac A Mole !</h2>').css({
+        color: '#fff',
+        'text-align': 'center',
+        padding: '10px 0 0 0'
+    });
+    new GameMenu(selector);
+}
+
+function GameMenu(selector){
+    selector.append('<div id="game_menu"><ul>Game menu<li id="start">Start</li></ul></div>');
+    $('#game_menu').css({
+        background: '#fff',
+        width: '50%',
+        height: '50%',
+        position: 'absolute',
+        bottom: '10%',
+        left: '25%'
+    });
+    $('#game_menu ul').css({
+        color: '#000'
+    });
+
+    $('#game_menu ul li').on('mouseover', function () {
+        $(this).css({
+            background: '#000',
+            color: '#fff',
+            cursor: 'pointer'
+        })
+    }).on('mouseout', function () {
+        $(this).css({
+            background: '#fff',
+            color: '#000',
+            cursor: 'pointer'
+        })
+    });
+
+    $('#start').on('click', function () {
+        $('#game_menu').remove();
     })
 }
 
